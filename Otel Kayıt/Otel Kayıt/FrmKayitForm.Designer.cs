e2e2570e41b136b07c7ab3a34d513cd30788b0c1
livaanurpulat <58486406+livaanurpulat@users.noreply.github.com>
2020-05-12 04:04:58 +0300

    deneme

--- Otel Kayıt/Otel Kayıt/FrmKayitForm.Designer.cs
+++ Otel Kayıt/Otel Kayıt/FrmKayitForm.Designer.cs
@@ -504,7 +504,7 @@
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "FrmKayitForm";
             this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
